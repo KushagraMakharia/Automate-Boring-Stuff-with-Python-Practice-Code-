f1dfcd8fe0efe0861d2e994a0432dd4d3fc3fb71
KushagraMakharia <kushagramakharia2@gmail.com>
2020-07-11 13:05:25 +0530

    table printer program

--- Table Printer/table_printer.py
+++ Table Printer/table_printer.py
@@ -3,3 +3,25 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
 ['dogs', 'cats', 'moose', 'goose']]
 
 def printTable(tableData):
+    row_length = []
+
+    for col in tableData:
+        row_length.append(len(col))
+
+    table_row = max(row_length)
+    table_column = len(tableData)
+
+
+    line_text = ""
+
+    for r in range(table_row):
+        for c in range(table_column):
+            try:
+                line_text = line_text + " " + tableData[c][r]
+            except IndexError:
+                line_text = line_text + " "
+        print(line_text)
+        line_text = ""
+
+
+printTable(tableData)
